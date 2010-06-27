e207902c8386739126ae37e0aeb810faa119a4ed
ivo <ivo@danihelka.net>
2010-06-27 18:02:28 +0200

    Used myerrata.min.js inside the bookmarklet.

--- app/static/js/bookmarklet.js
+++ app/static/js/bookmarklet.js
@@ -7,7 +7,7 @@
             start:true
         };
         e = d.createElement('script'); e.type = 'text/javascript';
-        e.src = w.MyErrata.host + '/static/js/myerrata.js';
+        e.src = w.MyErrata.host + '/static/js/myerrata.min.js';
         (d.getElementsByTagName('head')[0]||d.body).appendChild(e);
     }
 })(window, document, '0.1');
