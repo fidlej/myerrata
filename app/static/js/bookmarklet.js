5f5a79edb260182b1f9ccb5f39f8aa88ed323d86
ivo <ivo@danihelka.net>
2010-06-30 15:35:06 +0200

    Included the used bookmarklet version number.

--- app/static/js/bookmarklet.js
+++ app/static/js/bookmarklet.js
@@ -4,10 +4,11 @@
     } else {
         w.MyErrata = {
             host:'http://www.myerrata.com',
-            start:true
+            start:true,
+            version:'0.1'
         };
         e = d.createElement('script'); e.type = 'text/javascript';
         e.src = w.MyErrata.host + '/static/js/myerrata.min.js';
         (d.getElementsByTagName('head')[0]||d.body).appendChild(e);
     }
-})(window, document, '0.1');
+})(window, document, 0);
