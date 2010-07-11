b849c15185be64747feebacaaf4a136a1c9bb9dc
ivo <ivo@danihelka.net>
2010-07-11 11:33:22 +0200

    Allowed to click next to the checkbox.

--- app/src/formatting.py
+++ app/src/formatting.py
@@ -1,9 +1,15 @@
 
 from markupsafe import Markup
 
-def style_fix(fix):
+def attr_class(fix):
     if fix.gone:
-        return Markup(' class="gone"')
+        return Markup(' class="fix gone"')
+    else:
+        return Markup(' class="fix"')
+
+def attr_gone_checked(fix):
+    if fix.gone:
+        return Markup(' checked="checked"')
     else:
         return ''
 
